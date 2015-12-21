40d3e8be1315d58705fde3a457f8fa4850ca0942
AirSars <188561521@qq.com>
2015-12-21 23:18:35 +0800

    add
    base

---
+++ Fresh/Fresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fresh
+//
+//  Created by NADA_BM on 15/12/21.
+//  Copyright © 2015年 NADA_BM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
